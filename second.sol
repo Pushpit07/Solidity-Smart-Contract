6b23edf0ee6eb53dcb4a2fa7342e62c81ff1ab7a
Pushpit <pushpit07@gmail.com>
2021-05-07 18:43:36 +0530

    Create second.sol

---
+++ second.sol
@@ -0,0 +1,9 @@
+pragma solidity 0.5.1;
+
+contract MyContract {
+    string public constant value = "1 val";
+    bool public myBool = true;
+    int public myInt = -16; // can be negative
+    uint public myUint = 4; // cannot be negative
+    uint8 public myUint8 = 8;
+}
