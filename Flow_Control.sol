a19cefdf5f9ebbac10b7bac95f51a77455e4e9b4
Pushpit <pushpit07@gmail.com>
2021-05-09 20:25:03 +0530

    Create Flow_Control.sol

---
+++ Flow_Control.sol
@@ -0,0 +1,34 @@
+pragma solidity ^0.6.0;
+
+contract FlowControl {
+    uint[] public numbers = [1,2,3,4,5,6,7,8,9,10];
+    
+    address public owner;
+    
+    constructor() public {
+        owner = msg.sender;
+    }
+    
+    function countEvenNumbers() public view returns (uint) {
+        uint _count = 0;
+        
+        for(uint i=0; i<numbers.length; i++) {
+            if(isEvenNumber(numbers[i]))
+                _count++;
+        }
+        
+        return _count;
+    }
+    
+    function isEvenNumber(uint _number) public view returns (bool) {
+        if (_number % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    function isOwner() public view returns (bool) {
+        return (msg.sender == owner);
+    }
+}
